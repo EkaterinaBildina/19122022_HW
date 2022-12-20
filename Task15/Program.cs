28c5b2748cf088dd93181224de44c616c77e7ce0
EkaterinaBildina <kukresch@gmail.com>
2022-12-20 11:59:46 +0300

    Task15 Programm file write success

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -5,3 +5,20 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Write("Введите цифру дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0 || number > 7)
+{
+    Console.WriteLine("Ошибка ввода числа: в неделе 7 дней");
+}
+else
+    Console.WriteLine(DayNumber(number) ? "Да" : "Нет");
+
+bool DayNumber(int daynum)
+{
+    return daynum == 6 || daynum == 7;
+}
+
+
