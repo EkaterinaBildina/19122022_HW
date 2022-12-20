040de976ba0828cb3fe513a150c3f1e9a5291e64
EkaterinaBildina <kukresch@gmail.com>
2022-12-20 11:21:49 +0300

    Task13 Programm file completion

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -7,17 +7,22 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-int number = Convert.ToInt32(new Random().Next());
-
-if (number < 100) Console.WriteLine("третьей цифры нет");
-
+int number = new Random().Next();
 int count = number;
 
-while (count > 999)
+if (number < 100)
 {
-    count = number / 10;
+    Console.WriteLine("третьей цифры нет");
 }
-break;
+else
+{
+    while (count > 999)
+    {
+        count = count / 10;
 
+        if (count > 100 && count < 1000)
+            break;
+    }
 int result = count % 10;
 Console.WriteLine($"третья цифра числа {number} -> {result}");
+}
