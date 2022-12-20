9c288311bcd1da903d1ec0b7d9b09f8e6c1280a4
EkaterinaBildina <kukresch@gmail.com>
2022-12-20 10:35:28 +0300

    Task13 Programm code write

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет. 
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// Проверка:
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int number = Convert.ToInt32(new Random().Next());
+
+if (number < 100) Console.WriteLine("третьей цифры нет");
+
+int count = number;
+
+while (count > 999)
+{
+    count = number / 10;
+}
+break;
+
+int result = count % 10;
+Console.WriteLine($"третья цифра числа {number} -> {result}");
